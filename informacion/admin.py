23a478dfcd1d84bec5824df503185f07c3b7f7de
infosec-o4it <arincon@o4it.com>
2014-03-13 14:45:39 -0500

    intento de pep8

--- informacion/admin.py
+++ informacion/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from models import tipo, activo, area, usuario, niveles_acceso,datacenter
+from models import tipo, activo, area, usuario, niveles_acceso, datacenter
 from django.http import HttpResponse
 
 import csv
- 
- 
+
+
 def prep_field(obj, field):
     """ Returns the field as a unicode string. If the field is a callable, it
     attempts to call it first, without arguments.
@@ -12,19 +12,20 @@ def prep_field(obj, field):
     if '__' in field:
         bits = field.split('__')
         field = bits.pop()
- 
+
         for bit in bits:
             obj = getattr(obj, bit, None)
- 
+
             if obj is None:
                 return ""
- 
+
     attr = getattr(obj, field)
     output = attr() if callable(attr) else attr
     return unicode(output).encode('utf-8') if output else ""
- 
- 
-def export_csv_action(description="Export as CSV", fields=None, exclude=None, header=True):
+
+
+def export_csv_action(description="Export as CSV", \
+                      fields=None, exclude=None, header=True):
     """ This function returns an export csv action. """
     def export_as_csv(modeladmin, request, queryset):
         """ Generic csv export admin action.
@@ -33,24 +34,24 @@ def export_csv_action(description="Export as CSV", fields=None, exclude=None, he
         opts = modeladmin.model._meta
         field_names = [field.name for field in opts.fields]
         labels = []
- 
+
         if exclude:
             field_names = [f for f in field_names if f not in exclude]
- 
+
         elif fields:
             field_names = [field for field, _ in fields]
             labels = [label for _, label in fields]
- 
+
         response = HttpResponse(mimetype='text/csv')
         response['Content-Disposition'] = 'attachment; filename=%s.csv' % (
                 unicode(opts).replace('.', '_')
             )
- 
+
         writer = csv.writer(response)
- 
+
         if header:
             writer.writerow(labels if labels else field_names)
- 
+
         for obj in queryset:
             writer.writerow([prep_field(obj, field) for field in field_names])
         return response
@@ -58,10 +59,14 @@ def export_csv_action(description="Export as CSV", fields=None, exclude=None, he
     return export_as_csv
 
 # Register your models here.
+
+
 class activoAdmin(admin.ModelAdmin):
-    list_display = ('activo', 'tipo', 'propietario','responsable','confidencialidad','integridad','disponibilidad')
+    list_display = ('activo', 'tipo', \
+        'propietario', 'responsable', \
+        'confidencialidad', 'integridad', 'disponibilidad')
     search_fields = ('activo', 'propietario')
-    list_filter = ('area', 'ubicacion','responsable','tipo')
+    list_filter = ('area', 'ubicacion', 'responsable', 'tipo')
     actions = [
         export_csv_action("Export Sepecial Report",
             fields=[
@@ -87,19 +92,22 @@ class activoAdmin(admin.ModelAdmin):
         ),
     ]
 
+
 class areaAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'descripcion')
-    
+
+
 class usuarioAdmin(admin.ModelAdmin):
-    list_display = ('cargo','area')
+    list_display = ('cargo', 'area')
+
 
 class niveles_accesoAdmin(admin.ModelAdmin):
     list_display = ('activo', 'usuario')
     search_fields = ('activo', 'usuario')
-    
-admin.site.register(area,areaAdmin)
-admin.site.register(usuario,usuarioAdmin)
-admin.site.register(niveles_acceso,niveles_accesoAdmin)
+
+admin.site.register(area, areaAdmin)
+admin.site.register(usuario, usuarioAdmin)
+admin.site.register(niveles_acceso, niveles_accesoAdmin)
 admin.site.register(tipo)
 admin.site.register(datacenter)
-admin.site.register(activo,activoAdmin)
+admin.site.register(activo, activoAdmin)
--- informacion/models.py
+++ informacion/models.py
@@ -1,74 +1,87 @@
 from django.db import models
 
 # Create your models here.
+
+
 class tipo(models.Model):
     tipo = models.CharField(max_length=30)
+
     def __unicode__(self):
         return self.tipo
-    
+
+
 class area(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(null=True, blank=True)
+
     def __unicode__(self):
         return unicode(self.nombre)
-    
+
+
 class usuario(models.Model):
     cargo = models.CharField(max_length=100)
     telefono = models.CharField(max_length=12)
     area = models.ForeignKey(area)
+
     def __unicode__(self):
         return unicode(self.cargo)
 niveles = (
-           (0,"0"),
-           (1,"1"),
-           (2,"2"),
-           (3,"3"),
-           (4,"4"),
-           (5,"5"),
-           (6,"6"),
-           (7,"7"),
-           (8,"8"),
-           (9,"9"),
-           (10,"10"),
+           (0, "0"),
+           (1, "1"),
+           (2, "2"),
+           (3, "3"),
+           (4, "4"),
+           (5, "5"),
+           (6, "6"),
+           (7, "7"),
+           (8, "8"),
+           (9, "9"),
+           (10, "10"),
            )
-datacenters =(
-              ("TMBOG","Terramar Bogota"),
-              ("TMMiami","Terramar Miami"),
-              ("XO","XO Comunications"),
-              ("UNEMed","Une Medellin"),
-              ("CLL74","Oficina calle 74"),
-              ("OF Remotas","Oficinas Remotas"),
-              ("Externo","Externo"),
+datacenters = (
+              ("TMBOG", "Terramar Bogota"),
+              ("TMMiami", "Terramar Miami"),
+              ("XO", "XO Comunications"),
+              ("UNEMed", "Une Medellin"),
+              ("CLL74", "Oficina calle 74"),
+              ("OF Remotas", "Oficinas Remotas"),
+              ("Externo", "Externo"),
               )
+
+
 class datacenter(models.Model):
-    descripcion = models.CharField(max_length=10,choices=datacenters)
+    descripcion = models.CharField(max_length=10, choices=datacenters)
+
     def __unicode__(self):
         return unicode(self.descripcion)
-    
+
+
 class activo(models.Model):
     activo = models.CharField(max_length=30)
-    descripcion = models.TextField(null=True,blank=True)
+    descripcion = models.TextField(null=True, blank=True)
     tipo = models.ForeignKey(tipo)
     ubicacion = models.ManyToManyField(datacenter)
-    area = models.ForeignKey(area) 
-    propietario = models.ForeignKey(usuario, related_name = "propietario")
-    responsable = models.ForeignKey(usuario, related_name = "responsable")
+    area = models.ForeignKey(area)
+    propietario = models.ForeignKey(usuario, related_name="propietario")
+    responsable = models.ForeignKey(usuario, related_name="responsable")
     usuario = models.ForeignKey(usuario)
     funcion = models.TextField(null=True, blank=True)
     datos_personales = models.NullBooleanField()
     propia = models.NullBooleanField()
     terceros = models.NullBooleanField()
     clientes = models.NullBooleanField()
-    confidencialidad = models.IntegerField(choices=niveles,default=5)
-    integridad = models.IntegerField(choices=niveles,default=5)
-    disponibilidad = models.IntegerField(choices=niveles,default=5)
+    confidencialidad = models.IntegerField(choices=niveles, default=5)
+    integridad = models.IntegerField(choices=niveles, default=5)
+    disponibilidad = models.IntegerField(choices=niveles, default=5)
+
     def __unicode__(self):
         return self.activo
 
+
 class niveles_acceso(models.Model):
     activo = models.ForeignKey(activo)
-    usuario = models.ForeignKey(usuario) 
+    usuario = models.ForeignKey(usuario)
     leer = models.NullBooleanField()
     modificar = models.NullBooleanField()
     borrar = models.NullBooleanField()
-    unique_together = (activo,usuario)      
+    unique_together = (activo, usuario)
