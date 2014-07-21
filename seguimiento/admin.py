7a5ef31975a6773b19236421100b306fbf96166e
arincon <arincon@shiki.o4it.corp.local>
2014-07-21 18:52:14 -0500

    inicio del modulo de seguimiento de iso 27001

---
+++ seguimiento/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from models import seccion, control, numeral, punto, hallazgo
+# Register your models here.
+
+admin.site.register(seccion)
+admin.site.register(control)
+admin.site.register(numeral)
+admin.site.register(punto)
+admin.site.register(hallazgo)
---
+++ seguimiento/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from informacion.models import usuario
+# Create your models here.
+
+
+class seccion(models.Model):
+    codigo = models.CharField(max_length=4)
+    descripcion = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "secciones"
+
+    def __unicode__(self):
+        return str(self.codigo.encode("utf8"))
+
+
+class control(models.Model):
+    seccion = models.ForeignKey(seccion)
+    codigo = models.CharField(max_length=3)
+    descripcion = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "controles"
+
+    def __unicode__(self):
+        return str(str(self.seccion) + self.codigo)
+
+
+class numeral(models.Model):
+    control = models.ForeignKey(control)
+    codigo = models.CharField(max_length=3)
+    descripcion = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "numerales"
+
+    def __unicode__(self):
+        return str(str(self.control) + self.codigo)
+
+
+class punto(models.Model):
+    numeral = models.ForeignKey(numeral)
+    pregunta = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return str(str(self.numeral) + " " + self.pregunta[:35])
+nivel = (
+    (0, "0"),
+    (1, "1"),
+    (2, "2"),
+    (3, "3"),
+    (4, "4"),
+    (5, "5"),
+)
+
+
+class hallazgo(models.Model):
+    punto = models.ForeignKey(punto)
+    resumen = models.CharField(max_length=16)
+    usuario = models.ForeignKey(usuario)
+    descripcion = models.TextField(null=True, blank=True)
+    fecha = models.DateField()
+    nota = models.IntegerField(max_length=1, choices=nivel)
+
+    def __unicode__(self):
+        return str(str(self.punto) + self.resumen)
